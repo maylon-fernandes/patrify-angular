3ed5326c0a84f1b0568c7354ab8986f50206a7fe
keviwsilva <kwms1345@gmail.com>
2024-05-09 22:40:07 -0300

    filter

--- src/app/service/authguard.service.ts
+++ src/app/service/authguard.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { BackendService } from './backend.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthguardService implements CanActivate {
 
-    constructor(private router: Router) { }
+    constructor(private router: Router, private backendService: BackendService) { }
   
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
       const token = localStorage.getItem('token'); // Get token from local storage
@@ -14,6 +15,25 @@ export class AuthguardService implements CanActivate {
         this.router.navigate(['/login']); // Redirect to login if not found
         return false;
       }
+      // Verificar se o usuário está autenticado usando o token
+    else if (token) {
+
+      this.backendService.SignedUser(token)
+        .subscribe(
+          response => {
+            // Manipular a resposta bem-sucedida aqui (por exemplo, exibir mensagem de sucesso)
+            // console.log(response);
+            this.router.navigate(['/inventario']);
+          },
+          error => {
+            this.router.navigate(['/login']);
+            // Manipular erros de autenticação aqui (por exemplo, exibir mensagem de erro)
+            // console.error('Erro ao verificar autenticação do usuário:', error);
+          }
+        );
+    } else {
+      console.error('Token não encontrado no localStorage.');
+    }
   
       // If you have token validation logic, add it here
       // e.g., check token expiration, validity, etc.
