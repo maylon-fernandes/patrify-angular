c7d57b6810bb876b8fb8af145795b34d41b45a33
GuilhermeFMacedo <guilhermeferrazmacedo@gmail.com>
2024-05-22 20:45:59 -0300

    header

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  currentRoute: string = '';
 
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.currentRoute = this.router.url.substring(1).toUpperCase();
+    
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url.substring(1).toUpperCase();
+        console.log(this.currentRoute);
+      }
+    });
+  }
 }
