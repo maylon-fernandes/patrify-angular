6e2f1d091399da67428d195b8adf80c8e4ad8f04
keviwsilva <kwms1345@gmail.com>
2024-06-07 21:47:44 -0300

    grafico

--- src/app/pages/home/chart/chart.component.ts
+++ src/app/pages/home/chart/chart.component.ts
@@ -12,63 +12,96 @@ export class ChartComponent implements OnInit{
   patrimonios: any[] = [];
   filtros: any[] = []; 
   token: string | null = '';
+  tipospatr: any = [];
 
   constructor(private backendService: BackendService) {}
 
   ngOnInit(): void {
-    this.renderChart()
-
-    this.filterfunction()
-
+    this.filterfunction().then((tiposPatrimonios: any) => {
+      this.tipospatr = tiposPatrimonios;
+      console.log(this.tipospatr);
+      this.renderChart(this.tipospatr);
+    }).catch(error => {
+      console.error(error);
+    });
   }
-
-  filterfunction(){
-    
-    this.token = localStorage.getItem('token');
-    
-    if (this.token) {
-      this.backendService.Filter(this.filtros, this.token).subscribe(
-        response => {
-        this.patrimonios = response.patrimonios.map((patrimonio: any) => {
-        
-
-          return {
-            ...patrimonio,
-          };
+  
 
 
-        });
-
-        // Count the number of patrimonios
-        const patrimoniosCount = this.patrimonios.length;
-        console.log('Número de patrimônios retornados:', patrimoniosCount);
-      },
-      (error) => {
-        console.error('Error:', error);
+  filterfunction() {
+    return new Promise((resolve, reject) => {
+      this.token = localStorage.getItem('token');
+  
+      if (this.token) {
+        this.backendService.Filter(this.filtros, this.token).subscribe(
+          response => {
+            if (response.patrimonios) {
+              this.patrimonios = response.patrimonios.map((patrimonio: any) => {
+                return {
+                  ...patrimonio,
+                };
+              });
+  
+              // Contar o número de patrimônios
+              const tiposPatrimonios: any = [];
+  
+              this.patrimonios.forEach(patrimonio => {
+                let tipoEncontrado = tiposPatrimonios.find((tipo: { tipo: any; }) => tipo.tipo === patrimonio.patr_tipo);
+              
+                if (!tipoEncontrado) {
+                  tiposPatrimonios.push({
+                    tipo: patrimonio.patr_tipo,
+                    contagem: 1
+                  });
+                } else {
+                  tipoEncontrado.contagem++;
+                }
+              });
+  
+              resolve(tiposPatrimonios); // Resolve the Promise with the result
+            } else {
+              console.error('Erro ao recuperar patrimônios da API: ', response);
+              reject('Erro ao recuperar patrimônios da API');
+            }
+          },
+          (error) => {
+            console.error('Erro ao filtrar patrimônios: ', error);
+            reject('Erro ao filtrar patrimônios');
+          }
+        );
       }
-    );
+    });
   }
-}
+  
+  
 
-  renderChart(){
-   const myChart = new Chart("piechart", {
+
+  renderChart(tipospatr: any){
+    console.log(tipospatr)
+    const labels = tipospatr.map((tipo: { tipo: any; }) => tipo.tipo);
+    const data = tipospatr.map((tipo: { contagem: any; }) => tipo.contagem);
+
+   const myChart = new Chart("typechart", {
     type: 'doughnut',
     data: {
-      labels: ['PATRIMONIO'],
+      labels: labels,
       datasets: [{
-        label: '# of Votes',
-        data: [4],
-        borderWidth: 1
+        label: '',
+        data: data,
+        borderWidth: 1,
+        backgroundColor: [
+          '#380664',
+          '#970086',
+          '#b60084',
+          '#ff7131',
+          '#ff9e00'
+        ],
       }]
     },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
+    
   });
   }
 
 }
+
+
